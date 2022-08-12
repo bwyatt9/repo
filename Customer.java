e5dd1c875b6966c8fcaccf90d953f29da22ae78c
bwyatt9 <107454339+bwyatt9@users.noreply.github.com>
2022-08-11 22:53:40 -0400

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,29 @@
+
+public class Customer {
+String name;
+int age;
+public Customer(String n,int a) {
+	name=n;
+	age=a;
+}
+public Customer(Customer c) {
+	name=c.name;
+	age=c.age;
+}
+public String toString() {
+	return name + age;
+}
+
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+}
